32a349c94baa85e3d52db5acef68787005743a3d
Lorenzo <lferrario@byte-code.com>
2017-05-26 10:15:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = require('./lib/axios');
+
+var a = 'asjdlas';
+var main = (function () {
+  console.log('the answer is ' + a);
+});
+
+module.exports = main;
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import axios from 'axios';
+
+const a = 'asjdlas';
+export default () => {
+  console.log(`the answer is ${a}`);
+};
